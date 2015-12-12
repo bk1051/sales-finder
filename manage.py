4d71068b34f168a2b38f95ea063298c25a502297
Brian Karfunkel <bk1051@nyu.edu>
2015-12-12 14:03:45 -0500

    Init_db method

--- manage.py
+++ manage.py
@@ -5,7 +5,8 @@ This is the script for launching the application
 
 import os
 from app import create_app, db
-#from app.models import User, Role
+from app.models import Sale
+from app.data import SalesData
 from flask.ext.script import Manager, Shell
 #from flask.ext.migrate import Migrate, MigrateCommand
 
@@ -20,7 +21,7 @@ manager = Manager(app)
 #migrate = Migrate(app, db)
 
 def make_shell_context():
-    return dict(app=app) #, db=db, User=User, Role=Role)
+    return dict(app=app, db=db, Sale=Sale)
 
 # Add the shell command to the manager, so we can launch a shell
 # with the objects already created in the form of a context
@@ -32,17 +33,46 @@ manager.add_command("shell", Shell(make_context=make_shell_context))
 # available on the command line as a subcommand for manager.py
 @manager.command
 def test():
-	'''Run unit tests'''
-	import unittest
-	# Get all the tests in the "tests" package and run them
-	tests = unittest.TestLoader().discover('tests')
-	unittest.TextTestRunner(verbosity=2).run(tests)
+    '''Run unit tests'''
+    import unittest
+    # Get all the tests in the "tests" package and run them
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+
+def warn_user(message):
+    '''Warn user about database overwriting'''
+    if message is not None:
+        print message
+    answer = raw_input("Are you sure you want to do this? [y/n] ")
+    while answer.lower()[0] not in ('y', 'n'):
+        answer = raw_input("Please enter y or n. ")
+
+    return answer.lower()[0] == 'y'
+
 
 @manager.command
 def init_db():
-	pass
+    '''This will create the database from scratch'''
+    try:
+        if not warn_user("This will DESTROY all data in the database and replace with newly \ndownloaded data."):
+            print "Cancelled"
+            return
+    except EOFError:
+        print "\nInvalid response. Aborting."
+        return
+
+    print "\nInitializing database..."
+    db.drop_all()
+    db.create_all()
+    salesdata = SalesData()
+    salesdata.create_from_scratch(db)
+
 
 
 
 if __name__ == '__main__':
-    manager.run()
+    try:
+        manager.run()
+    except KeyboardInterrupt:
+        print "\nQUITTING"
