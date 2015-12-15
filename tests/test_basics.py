b941600eb7d6b997980937340d21dd72d68fa455
Brian Karfunkel <bk1051@nyu.edu>
2015-12-15 14:14:54 -0500

    Done test basics

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -3,30 +3,31 @@ Module to do basic testing for the Flask web app.
 '''
 import unittest
 from flask import current_app
-from app import create_app
-#import pprint
+from sales_finder import create_app
 
 class BasicTestCase(unittest.TestCase):
+    '''Test case for basic functions'''
 
-	def setUp(self):
-		# Create an app instance using the testing configuration
-		self.app = create_app('testing')
+    def setUp(self):
+        '''Set up before each test'''
+        # Create an app instance using the testing configuration
+        self.app = create_app('testing')
 
-		# Creating the app_context means the tests will have
-		# access to current_app
-		self.app_context = self.app.app_context()
-		self.app_context.push()
+        # Creating the app_context means the tests will have
+        # access to current_app
+        self.app_context = self.app.app_context()
+        self.app_context.push()
 
 
 
-	def tearDown(self):
-		#db.session.remove()
-		#db.drop_all()
-		#manage.drop_table()
-		self.app_context.pop()
+    def tearDown(self):
+        '''Clean up after test'''
+        self.app_context.pop()
 
-	def test_app_exists(self):
-		self.assertFalse(current_app is None)
+    def test_app_exists(self):
+        '''Test that app exists'''
+        self.assertFalse(current_app is None)
 
-	def test_app_is_testing(self):
-		self.assertTrue(current_app.config['TESTING'])
+    def test_app_is_testing(self):
+        '''Test that config TESTING variable is set correctly'''
+        self.assertTrue(current_app.config['TESTING'])
